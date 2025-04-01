a5f8d9f39ac9e37227d77b00cfcd2637b9153400
unknown <mykola.denysenko@infis.cz>
2025-04-01 08:36:45 +0200

    first commit

--- 5.py
+++ 5.py
@@ -1,9 +1,4 @@
-N = int(input("Zadejte cislo N "))
-s = 0
-for i in range(1, N + 1):
-    if i < N:
-        print(i, end="+")
-    else:
-        print(i, end="=")
-    s += i
-print(s)
+vek = input("Napiste vas vek")
+jmeno = input("Napiste jmeno")
+
+print(f"{jmeno} {vek}")
